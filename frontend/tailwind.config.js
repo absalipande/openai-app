5dccd8e647d21e528bb4ee59f79a9b93d73c7ddb
Amiel Brencis Salipande <absalipande@gmail.com>
2023-05-20 16:43:51 +0800

    docs: Setup folder structure

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        user: '#333541',
+        AI: '#444654',
+      },
+    },
   },
   plugins: [],
-}
+};
