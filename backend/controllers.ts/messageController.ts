fe6a04928c79b8ab1ebe3eb2b4bd644994f383f8
Amiel Brencis Salipande <absalipande@gmail.com>
2023-05-20 16:01:36 +0800

    feat: Added messageController.ts about handling message and generate chat response.
    - Implement request handling for incoming messages
    - Utilize the generateChatReponse from openaiService.ts
    - Add error handling and appropriate error response

---
+++ backend/controllers.ts/messageController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { generateChatResponse } from '../services/openaiServices';
+
+export async function handleMessage(
+  req: Request,
+  res: Response
+): Promise<void> {
+  const message = req.body.message;
+
+  try {
+    const response = await generateChatResponse(message);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: 'An error occurred while processing the message.' });
+  }
+}
