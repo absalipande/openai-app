7ac7db5f2633b03a8fe037cbfb46907c32afb179
Amiel Brencis Salipande <absalipande@gmail.com>
2023-05-20 16:06:20 +0800

    feat: Added routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import messageRoutes from './routes/messageRoutes';
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,8 @@ const port = process.env.PORT || 3070;
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/', messageRoutes);
 
 app.listen(port, () => {
   console.log(`Server is up and running on port: ${port}`);
