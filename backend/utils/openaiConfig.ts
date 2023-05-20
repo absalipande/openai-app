5172ddb04a20284b343d48e15e90a04143748dcf
Amiel Brencis Salipande <absalipande@gmail.com>
2023-05-20 15:45:03 +0800

    feat: Setup the API key from OpenAI

---
+++ backend/utils/openaiConfig.ts
@@ -0,0 +1,17 @@
+import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const openaiApiKey = process.env.OPENAI_API_KEY;
+
+if (!openaiApiKey) {
+  throw new Error('Missing OPENAI_API_KEY');
+}
+
+const configuration = new Configuration({
+  apiKey: openaiApiKey,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default openai;
