ffe38826c51d2464d13d7b8b7a21a6ac29229079
Amiel Brencis Salipande <absalipande@gmail.com>
2023-05-20 17:06:40 +0800

    feat: Implemented a typing effect

---
+++ frontend/components/TypingEffect.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+
+const TypingEffect = ({ text }: { text: string }) => {
+  const [displayText, setDisplayText] = useState('');
+  const typingRef = useRef<number | null>(null);
+  const typingSpeed = 30; // Adjust the typing speed as needed
+
+  useEffect(() => {
+    let currentIndex = 0;
+
+    const typeNextCharacter = () => {
+      if (currentIndex < text.length) {
+        setDisplayText((prevText) => prevText + text.charAt(currentIndex));
+        currentIndex++;
+        typingRef.current = window.setTimeout(typeNextCharacter, typingSpeed);
+      }
+    };
+
+    typeNextCharacter();
+
+    return () => {
+      // Cleanup function to cancel the typing effect if component unmounts
+      if (typingRef.current) {
+        clearTimeout(typingRef.current);
+      }
+    };
+  }, [text]);
+
+  return <>{displayText}</>;
+};
+
+export default TypingEffect;
